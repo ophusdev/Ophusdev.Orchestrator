f6049d89feba153c92e9577f670e32feb8cdc86f
OphusDev <53277151+ophusdev@users.noreply.github.com>
2025-07-21 22:48:15 +0200

    chore: calculate  a sample amount of PricePerNight into BookingService
    use Inventory.HttpCLient to retrieve in sync information about room

--- Ophusdev.Orchestrator.Business/Services/BookingService.cs
+++ Ophusdev.Orchestrator.Business/Services/BookingService.cs
@@ -1,58 +1,61 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using Inventory.ClientHttp.Abstraction;
+using Inventory.Shared;
+using Microsoft.Extensions.Logging;
+using Ophusdev.Kafka.Abstraction;
 using Ophusdev.Orchestrator.Business.Abstraction;
 using Ophusdev.Orchestrator.Shared;
+using Ophusdev.Orchestrator.Shared.Exceptions;
 using Ophusdev.Orchestrator.Shared.Models;
 using Orchestrator.Repository.Abstraction;
 using Orchestrator.Repository.Model;
-
-using Inventory.ClientHttp.Abstraction;
-using Inventory.Shared;
-using Ophusdev.Orchestrator.Shared.Exceptions;
 
 namespace Ophusdev.Orchestrator.Business.Services
 {
     public class BookingService : IBookingService
     {
+        private readonly IKafkaProducer _kafkaProducer;
         private readonly IRepository _bookingRepository;
-        private readonly IBookingSagaOrchestrator _sagaOrchestrator;
         private readonly ILogger<BookingService> _logger;
         private readonly IClientHttp _clientHttp;
+        private readonly ITopicTranslator _topicTranslator;
 
-        public BookingService(IRepository bookingRepository,
-            IBookingSagaOrchestrator sagaOrchestrator,
+        public BookingService(
+            IKafkaProducer kafkaProducer,
+            IRepository bookingRepository,
             ILogger<BookingService> logger,
+            ITopicTranslator topicTranslator,
             IClientHttp clientHttp)
         {
+            _kafkaProducer = kafkaProducer;
             _bookingRepository = bookingRepository;
-            _sagaOrchestrator = sagaOrchestrator;
             _logger = logger;
             _clientHttp = clientHttp;
+            _topicTranslator = topicTranslator;
         }
 
-        private async Task<bool> ExistsRoom(int roomId)
+        private async Task<RoomDto?> ReadRoomSync(int roomId)
         {
             try
             {
                 RoomDto? room = await _clientHttp.ReadRoomAsync(roomId);
 
-                if (room != null)
-                {
-                    return true;
-                }
+                return room;
             }
             catch (Exception e)
             {
                 _logger.LogError("Error to retrive info about room: room={room}, message={message}", roomId, e.Message);
             }
 
-            return false;
+            return null;
         }
 
         public async Task<BookingResponse> CreateBookingAsync(BookingInsertDto request)
         {
             string sagaId = Guid.NewGuid().ToString();
 
-            if (! await ExistsRoom(request.RoomId))
+            RoomDto? room = await ReadRoomSync(request.RoomId);
+
+            if (room == null)
             {
                 throw new RoomNotFoundException("Room is not available");
             }
@@ -62,7 +65,19 @@ namespace Ophusdev.Orchestrator.Business.Services
 
             _logger.LogInformation("Create saga transaction, sagaId={sagaId}", sagaId);
 
-            await _sagaOrchestrator.StartSagaAsync(sagaId, booking);
+            var inventoryRequest = new InventoryRequest
+            {
+                SagaId = sagaId,
+                BookingId = booking.BookingId,
+                RoomId = booking.RoomId,
+                CheckInDate = booking.CheckInDate,
+                CheckOutDate = booking.CheckOutDate,
+                GuestId = booking.GuestId
+            };
+
+            string topicName = _topicTranslator.GetTopicName("TOPIC_INVENTORY_REQUEST");
+
+            await _kafkaProducer.ProduceAsync(topicName, inventoryRequest);
 
             return new BookingResponse
             {
@@ -74,17 +89,88 @@ namespace Ophusdev.Orchestrator.Business.Services
 
         public async Task ProcessInventoryRequestAsync(InventoryResponse message)
         {
-            await _sagaOrchestrator.HandleInventoryResponseAsync(message);
+            var booking = await _bookingRepository.GetByBookingIdAsync(message.BookingId);
+
+            if (booking == null) return;
+
+            if (message.Success)
+            {
+                booking.Status = BookingStatus.InventoryConfirmed;
+                await _bookingRepository.UpdateAsync(booking);
+
+                RoomDto? room = await ReadRoomSync(booking.RoomId);
+
+                // We have a strange business in our hotel
+                decimal pricePerNight = room.PricePerNight * (decimal)1.5;
+
+                var paymentRequest = new PaymentRequest
+                {
+                    SagaId = message.SagaId,
+                    BookingId = message.BookingId,
+                    GuestId = booking.GuestId,
+                    Amount = pricePerNight,
+                    CreditCardNumber = booking.CreditCardNumber
+                };
+
+                string topicName = _topicTranslator.GetTopicName("TOPIC_PAYMENT_REQUEST");
+
+                await _kafkaProducer.ProduceAsync(topicName, paymentRequest);
+            }
+            else
+            {
+                _logger.LogWarning("Reserve room failed, abort booking");
+
+                booking.Status = BookingStatus.Failed;
+                await _bookingRepository.UpdateAsync(booking);
+            }
         }
 
         public async Task ProcessPaymentRequestAsync(PaymentResponse message)
         {
-            await _sagaOrchestrator.HandlePaymentResponseAsync(message);
+             var booking = await _bookingRepository.GetByBookingIdAsync(message.BookingId);
+
+            if (booking == null) return;
+
+            if (message.Success)
+            {
+                // TODO: chiamare un altro servizio per scopi didattici
+                _logger.LogInformation("Payment success, book room={roomId}", booking.RoomId);
+
+                booking.Status = BookingStatus.Paid;
+                
+                await _bookingRepository.UpdateAsync(booking);
+            }
+            else
+            {
+                _logger.LogWarning("Payment failed, abort booking");
+
+                booking.Status = BookingStatus.Failed_Payment;
+                
+                await _bookingRepository.UpdateAsync(booking);
+
+                _logger.LogWarning("Payment failed, start compensation on inventory");
+
+                await CompensateInventoryAsync(booking.SagaId, message.BookingId);
+            }
         }
 
         public async Task<BookingItem> GetBookingAsync(string bookingId)
         {
             return await _bookingRepository.GetByBookingIdAsync(bookingId);
+        }
+
+        private async Task CompensateInventoryAsync(string sagaId, string bookingId)
+        {
+            var compensationRequest = new CompensationRequest
+            {
+                SagaId = bookingId,
+                BookingId = bookingId,
+                Type = CompensationType.Inventory
+            };
+
+            string topicName = _topicTranslator.GetTopicName("TOPIC_COMPENSATION_REQUEST");
+
+            await _kafkaProducer.ProduceAsync(topicName, compensationRequest);
         }
     }
 }
